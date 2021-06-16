314d02648ec1618997987b7c36cd87fef6f421e2
Cosmin Stanjic <cosmin.stanjic@amdaris.com>
2021-06-17 00:42:18 +0300

    add ListGroup component and implement genre filtering

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import Like from './common/like';
+import ListGroup from './common/listGroup';
 import Pagination from './common/pagination';
 import { getMovies } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
 import { paginate } from '../../src/utils/paginate';
 
-export class Movies extends Component {
+class Movies extends Component {
 	state = {
-		movies: getMovies(),
+		movies: [],
+		genres: [],
 		currentPage: 1,
 		pageSize: 4,
+		selectedGenre: null,
 	};
+
+	componentDidMount() {
+		const genres = [{ name: 'All Genres' }, ...getGenres()];
+
+		this.setState({ movies: getMovies(), genres });
+	}
 
 	handleDelete = (movie) => {
 		const movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -28,60 +38,83 @@ export class Movies extends Component {
 		this.setState({ currentPage: page });
 	};
 
+	handleGenreSelect = (genre) => {
+		this.setState({ selectedGenre: genre, currentPage: 1 });
+	};
+
 	render() {
 		const { length: count } = this.state.movies;
-		const { movies: allMovies, pageSize, currentPage } = this.state;
+		const {
+			movies: allMovies,
+			pageSize,
+			currentPage,
+			selectedGenre,
+		} = this.state;
 
 		if (count === 0) return <p>There are no movies in the database.</p>;
 
-		const movies = paginate(allMovies, currentPage, pageSize);
-		console.log(movies);
+		const filtered =
+			selectedGenre && selectedGenre._id
+				? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+				: allMovies;
+
+		const movies = paginate(filtered, currentPage, pageSize);
+
 		return (
-			<>
-				<p>Showing {count} movies in the database.</p>
-				<table className="table">
-					<thead>
-						<tr>
-							<th>Title</th>
-							<th>Genre</th>
-							<th>Stock</th>
-							<th>Rate</th>
-							<th></th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{movies.map((movie) => (
-							<tr key={movie._id}>
-								<td>{movie.title}</td>
-								<td>{movie.genre.name}</td>
-								<td>{movie.numberInStock}</td>
-								<td>{movie.dailyRentalRate}</td>
-								<td>
-									<button
-										onClick={() => this.handleDelete(movie)}
-										className="btn btn-danger btn-sm"
-									>
-										Delete
-									</button>
-								</td>
-								<td>
-									<Like
-										liked={movie.liked}
-										onClick={() => this.handleLike(movie)}
-									/>
-								</td>
+			<div className="row">
+				<div className="col-3">
+					<ListGroup
+						items={this.state.genres}
+						selectedItem={this.state.selectedGenre}
+						onItemSelect={this.handleGenreSelect}
+					/>
+				</div>
+				<div className="col">
+					<p>Showing {filtered.length} movies in the database.</p>
+					<table className="table">
+						<thead>
+							<tr>
+								<th>Title</th>
+								<th>Genre</th>
+								<th>Stock</th>
+								<th>Rate</th>
+								<th></th>
+								<th></th>
 							</tr>
-						))}
-					</tbody>
+						</thead>
+						<tbody>
+							{movies.map((movie) => (
+								<tr key={movie._id}>
+									<td>{movie.title}</td>
+									<td>{movie.genre.name}</td>
+									<td>{movie.numberInStock}</td>
+									<td>{movie.dailyRentalRate}</td>
+									<td>
+										<button
+											onClick={() => this.handleDelete(movie)}
+											className="btn btn-danger btn-sm"
+										>
+											Delete
+										</button>
+									</td>
+									<td>
+										<Like
+											liked={movie.liked}
+											onClick={() => this.handleLike(movie)}
+										/>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
 					<Pagination
-						itemsCount={count}
+						itemsCount={filtered.length}
 						pageSize={pageSize}
 						currentPage={currentPage}
 						onPageChange={this.handlePageChange}
 					/>
-				</table>
-			</>
+				</div>
+			</div>
 		);
 	}
 }
